f947200183823472a3aff54647e1e6e0b05d0bff
karolina_rezkova <karolina.rezkova@gmail.com>
2024-09-10 18:34:35 +0200

    Solve Day 3 challenge

--- day03_Conditions.py
+++ day03_Conditions.py
@@ -7,6 +7,21 @@ import re
 import sys
 
 
+def is_weird(n: int) -> bool:
+    if n % 2 == 1:
+        return True
+    elif 6 <= n <= 20:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     N = int(input().strip())
+
+    assert 1 <= N <= 100
+
+    if is_weird(N):
+        print("Weird")
+    else:
+        print("Not Weird")
