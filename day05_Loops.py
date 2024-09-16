692ffbebd1196c8124defcf4bec293e6ec6c79f4
karolina_rezkova <karolina.rezkova@gmail.com>
2024-09-16 17:02:44 +0200

    Solve Day 5 Challenge

--- day05_Loops.py
+++ day05_Loops.py
@@ -1,11 +1,14 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
+RESULT_STRING_TEMPLATE = "{multiplier} x {multiplicand} = {product}"
+
+
+def print_multiples(multiplier: int):
+    for i in range(1, 11):
+        print(RESULT_STRING_TEMPLATE.format(multiplier=multiplier, multiplicand=i, product=i*n))
 
 
 if __name__ == '__main__':
     n = int(input().strip())
+
+    print_multiples(n)
