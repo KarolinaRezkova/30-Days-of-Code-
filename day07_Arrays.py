eeba0025926b3fa60c506b77e70c85b160b2a5c4
karolina_rezkova <karolina.rezkova@gmail.com>
2024-09-16 20:21:11 +0200

    Solve Day 7 Challenge

--- day07_Arrays.py
+++ day07_Arrays.py
@@ -7,7 +7,18 @@ import re
 import sys
 
 
+def print_in_reversed_order(array: list[int]):
+    arr.reverse()
+    print(" ".join(str(item) for item in arr))
+    pass
+
+
 if __name__ == '__main__':
     n = int(input().strip())
-
     arr = list(map(int, input().rstrip().split()))
+
+    assert 1 <= n <= 1_000
+    assert 1 <= max(arr) <= 10_000
+    assert len(arr) == n
+    
+    print_in_reversed_order(arr)
