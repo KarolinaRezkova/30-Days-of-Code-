a87edac258ab2937cc48dda01f24c92205a485f4
karolina_rezkova <karolina.rezkova@gmail.com>
2024-09-16 22:12:11 +0200

    Solve Day 9 Challenge

--- day09_Recursion.py
+++ day09_Recursion.py
@@ -1,10 +1,6 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
 
 
 #
@@ -13,9 +9,11 @@ import sys
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER n as parameter.
 #
-def factorial(n: int) -> int:
-    pass
-    # Write your code here
+def factorial(number: int) -> int:
+    if number <= 1:
+        return 1
+    else:
+        return number * (factorial(number - 1))
 
 
 if __name__ == '__main__':
@@ -25,6 +23,7 @@ if __name__ == '__main__':
 
     result = factorial(n)
 
+    #print(result)
     fptr.write(str(result) + '\n')
 
     fptr.close()
