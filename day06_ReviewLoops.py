9490044c2ae605855ad79cd451e5b70203ac4347
karolina_rezkova <karolina.rezkova@gmail.com>
2024-09-16 18:44:38 +0200

    Solve Day 6 Challenge

--- day06_ReviewLoops.py
+++ day06_ReviewLoops.py
@@ -1,1 +1,19 @@
+#!/bin/python3
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+def process_input(n: int):
+    for i in range(n):
+        line = input()
+        assert 2 <= len(line) <= 10_000
+
+        even = line[0::2]
+        odd = line[1::2]
+        print(even + " " + odd)
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    assert 1 <= n <= 10
+
+    process_input(n)
