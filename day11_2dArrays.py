28c46e4c20fac050a320018231fff84d7e63be60
karolina_rezkova <karolina.rezkova@gmail.com>
2024-09-25 16:33:05 +0200

    Solve Day 11 Challenge without numpy

--- day11_2dArrays.py
+++ day11_2dArrays.py
@@ -1,20 +1,19 @@
 #!/bin/python3
 
-import numpy as np
-
 INPUT_SIZE = 6
-HOURGLASS = np.array([[1, 1, 1],
-                      [0, 1, 0],
-                      [1, 1, 1]])
+HOURGLASS = [[1, 1, 1],
+             [0, 1, 0],
+             [1, 1, 1]]
 
 MIN_HOURGLASS_SUM = - 9 * 7
 
 
-def calculate_hourglass_sum(numpy_array: np.ndarray) -> int:
-    assert np.size(numpy_array) == np.size(HOURGLASS)
-    element_wise_product = numpy_array * HOURGLASS
-    hourglass_sum = int(np.sum(element_wise_product))
-    return hourglass_sum
+def calculate_hourglass_sum(input_array: list[list[int]], index_x: int, index_y: int) -> int:
+    current_sum = 0
+    for i in range(3):
+        for j in range(3):
+            current_sum += HOURGLASS[i][j] * input_array[index_x + i][index_y + j]
+    return current_sum
 
 
 if __name__ == '__main__':
@@ -25,13 +24,12 @@ if __name__ == '__main__':
         arr.append(list(map(int, input().rstrip().split())))
 
     # init variables
-    numpy_arr = np.array(arr)
     max_hour_glass_sum = MIN_HOURGLASS_SUM
 
     # calculate maximal hourglass sum
-    for x_index in range(INPUT_SIZE - 3):
-        for y_index in range(INPUT_SIZE - 3):
-            current_hourglass_sum = calculate_hourglass_sum(numpy_arr[x_index:x_index + 3, y_index:y_index + 3])
+    for x_index in range(INPUT_SIZE - 2):
+        for y_index in range(INPUT_SIZE - 2):
+            current_hourglass_sum = calculate_hourglass_sum(arr, x_index, y_index)
             max_hour_glass_sum = max(max_hour_glass_sum, current_hourglass_sum)
 
     # print result
