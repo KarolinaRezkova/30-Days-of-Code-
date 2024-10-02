aa3eafc16d1fae056e3c2ed914b33c0d097378f9
karolina_rezkova <karolina.rezkova@gmail.com>
2024-10-02 17:27:32 +0200

    Solve Day 12 Challenge

--- day12_Inheritance.py
+++ day12_Inheritance.py
@@ -10,29 +10,47 @@ class Person:
 
 
 class Student(Person):
-    pass
 
+    # Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self, firstName: str, lastName: str, idNumber: int, scores: list[int]):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
 
-# Class Constructor
-#
-#   Parameters:
-#   firstName - A string denoting the Person's first name.
-#   lastName - A string denoting the Person's last name.
-#   id - An integer denoting the Person's ID number.
-#   scores - An array of integers denoting the Person's test scores.
-#
-# Write your constructor here
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self) -> str:
+        average_score = sum(self.scores) / len(self.scores)
+        assert 0 <= average_score <= 100, "Average out of bounds, should be 0 <= avg(scores) <= 100."
 
+        match average_score:
+            case avg if 90 <= avg <= 100:
+                return 'O'
+            case avg if 80 <= avg < 90:
+                return 'E'
+            case avg if 70 <= avg < 80:
+                return 'A'
+            case avg if 55 <= avg < 70:
+                return 'P'
+            case avg if 40 <= avg < 55:
+                return 'D'
+            case avg if avg < 40:
+                return 'T'
 
-#   Function Name: calculate
-#   Return: A character denoting the grade.
-#
-# Write your function here
 
 line = input().split()
 firstName = line[0]
 lastName = line[1]
-idNum = line[2]
+idNum = int(line[2])
 numScores = int(input())  # not needed for Python
 scores = list(map(int, input().split()))
 s = Student(firstName, lastName, idNum, scores)
